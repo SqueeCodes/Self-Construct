139bc2d534dff7d16cf78facd4e3bba10ab08006
David <itsmesquee@gmail.com>
2024-08-20 13:24:18 -0600

    initializing enviornment

---
+++ src/features/editor/hooks/use-editor.ts
@@ -0,0 +1,15 @@
+import { useCallback } from "react";
+
+export const useEditor = () => {
+  const init = useCallback(({
+    initialCanvas,
+    initialContainer,
+  }: {
+    initialCanvas: any;
+    initialContainer: HTMLDivElement,
+  }) => {
+    console.log("Initializing editor")
+  }, []);
+
+  return { init }
+};
