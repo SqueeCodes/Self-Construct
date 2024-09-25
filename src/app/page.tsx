051114f4934fe0fa7b60cd1fa28be293baa72acc
David <itsmesquee@gmail.com>
2024-09-25 12:35:38 -0600

    added some authentications and linked db for storing users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { auth } from "../auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      
-      <Button size="lg">Click me!</Button>
+      {JSON.stringify(session)}
     </div>
   );
 }
