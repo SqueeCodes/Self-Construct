9eef9eb35ad5ee1e8597b15d3f94e0df5fcad1d3
David <itsmesquee@gmail.com>
2024-10-01 12:29:35 -0600

    added more credential options for login

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,43 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { JWT } from "next-auth/jwt";
 
 import { db } from "./db/drizzle";
- 
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string | undefined;
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [GitHub],
+  providers: [GitHub, Google],
   pages: {
     signIn: "/sign-in",
     error: "/sign-in"
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    session({ session, token }) {
+      if (token.id) {
+        session.user.id = token.id;
+      }
+
+      return session;
+
+    },
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    }
+
   }
 });
