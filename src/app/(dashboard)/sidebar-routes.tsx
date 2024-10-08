bbe303b6b8be447d261f0cfe4893054654887c1e
David <itsmesquee@gmail.com>
2024-10-08 17:34:54 -0600

    added home page styling and functionalities

---
+++ src/app/(dashboard)/sidebar-routes.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { CreditCard, Crown, CrownIcon, Home, MessageCircleQuestion } from "lucide-react";
+import { Button } from "../../components/ui/button";
+import { Separator } from "../../components/ui/separator";
+import { SidebarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
+
+export const SidebarRoutes = () => {
+  const pathname = usePathname();
+  return (
+    <div className="flex flex-col gap-y-4 flex-1">
+      <div className="px-4">
+        <Button
+          className="w-full rounded-xl border-none hover:bg-white hover:opacity-75 transition"
+          variant="outline"
+          size="lg"
+        >
+          <Crown className="mr-2 size-4 fill-yellow-500 text-yellow-500" />
+          Upgrade to Image AI Pro
+        </Button>
+      </div>
+      <div className="px-3">
+        <Separator />
+      </div>
+      <ul className="flex flex-col gap-y-1 px-3">
+        <SidebarItem
+          href="/"
+          icon={Home}
+          label="Home"
+          isActive={pathname === "/"}
+        />
+      </ul>
+      <div className="px-3">
+        <Separator />
+      </div>
+      <ul className="flex flex-col gap-y-1 px-3">
+        <SidebarItem
+          href={pathname}
+          icon={CreditCard}
+          label="Billing"
+          onClick={() => {}}
+        />
+        <SidebarItem
+          href="mailto:actuallydavid96@gmail.com"
+          icon={MessageCircleQuestion}
+          label="Get Help"
+          onClick={() => {}}
+        />
+      </ul>
+    </div>
+  );
+};
