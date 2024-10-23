f9203d2ce80e712cd3e39ec1a2cfe076e6d3c352
David <itsmesquee@gmail.com>
2024-10-23 12:44:34 -0600

    added some more functionalities to home page projects

--- src/app/(dashboard)/projects-section.tsx
+++ src/app/(dashboard)/projects-section.tsx
@@ -1,65 +1,106 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React from "react";
-
-import { AlertTriangleIcon, FileIcon, Loader2, Search } from "lucide-react";
-
-import {
-  Table,
-  TableRow,
-  TableBody,
-  TableCell,
-} from "../../components/ui/table";
-
+import { useRouter } from "next/navigation";
+import { formatDistanceToNow } from "date-fns";
+import { 
+  AlertTriangle, 
+  CopyIcon, 
+  FileIcon, 
+  Loader, 
+  MoreHorizontal, 
+  Search,
+  Trash
+} from "lucide-react";
 import { useGetProjects } from "../../features/projects/api/use-get-projects";
+import { Table, TableBody, TableCell, TableRow } from "../../components/ui/table";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../../components/ui/dropdown-menu";
+import { Button } from "../../components/ui/button";
+
 
 export const ProjectsSection = () => {
+  // const [ConfirmDialog, confirm] = useConfirm(
+  //   "Are you sure?",
+  //   "You are about to delete this project.",
+  // );
+  // const duplicateMutation = useDuplicateProject();
+  // const removeMutation = useDeleteProject();
   const router = useRouter();
 
-  const { data, status, fetchNextPage, isFetchingNextPage, hasNextPage } =
-    useGetProjects();
+  // const onCopy = (id: string) => {
+  //   duplicateMutation.mutate({ id });
+  // };
+
+  // const onDelete = async (id: string) => {
+  //   const ok = await confirm();
+
+  //   if (ok) {
+  //     removeMutation.mutate({ id });
+  //   }
+  // };
+
+  const {
+    data,
+    status,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useGetProjects();
 
   if (status === "pending") {
     return (
       <div className="space-y-4">
-        <h3 className="font-semibold text-lg">Recent Projects</h3>
+        <h3 className="font-semibold text-lg">
+          Recent projects
+        </h3>
         <div className="flex flex-col gap-y-4 items-center justify-center h-32">
-          <Loader2 className="size-6 animate-spin text-muted-foreground" />
+          <Loader className="size-6 animate-spin text-muted-foreground" />
         </div>
       </div>
-    );
+    )
   }
 
   if (status === "error") {
     return (
       <div className="space-y-4">
-        <h3 className="font-semibold text-lg">Recent Projects</h3>
+        <h3 className="font-semibold text-lg">
+          Recent projects
+        </h3>
         <div className="flex flex-col gap-y-4 items-center justify-center h-32">
-          <AlertTriangleIcon className="size-6 text-muted-foreground text-red-400 animate-bounce" />
-          <p className="text-muted-foreground text-sm animate-pulse">
-            Failed to load Projects :(
+          <AlertTriangle className="size-6 text-muted-foreground" />
+          <p className="text-muted-foreground text-sm">
+            Failed to load projects
           </p>
         </div>
       </div>
-    );
+    )
   }
 
-  if (!data.pages.length) {
+  if (
+    !data.pages.length ||
+    !data.pages[0].data.length
+  ) {
     return (
       <div className="space-y-4">
-        <h3 className="font-semibold text-lg">Recent Projects</h3>
+        <h3 className="font-semibold text-lg">
+          Recent projects
+        </h3>
         <div className="flex flex-col gap-y-4 items-center justify-center h-32">
           <Search className="size-6 text-muted-foreground" />
-          <p className="text-muted-foreground text-sm">No Projects Found :(</p>
+          <p className="text-muted-foreground text-sm">
+            No projects found
+          </p>
         </div>
       </div>
-    );
+    )
   }
 
   return (
-    <div className="space-y-4">
-      <h3 className="font-semibold text-lg">Recent Projects</h3>
+    <div className="space-y-4"> 
+      {/* <ConfirmDialog /> */}
+      <h3 className="font-semibold text-lg">
+        Recent projects
+      </h3>
       <Table>
         <TableBody>
           {data.pages.map((group, i) => (
@@ -67,23 +108,56 @@ export const ProjectsSection = () => {
               {group.data.map((project) => (
                 <TableRow key={project.id}>
                   <TableCell
-                    className="font-medium flex items-center gap-x-2 cursor-pointer"
                     onClick={() => router.push(`/editor/${project.id}`)}
+                    className="font-medium flex items-center gap-x-2 cursor-pointer"
                   >
                     <FileIcon className="size-6" />
                     {project.name}
                   </TableCell>
                   <TableCell
-                    className="hidden md:table-cell cursor-pointer"
                     onClick={() => router.push(`/editor/${project.id}`)}
+                    className="hidden md:table-cell cursor-pointer"
                   >
                     {project.width} x {project.height} px
                   </TableCell>
                   <TableCell
-                    className="hidden md:table-cell cursor-pointer"
                     onClick={() => router.push(`/editor/${project.id}`)}
+                    className="hidden md:table-cell cursor-pointer"
                   >
-                    {project.width} x {project.height} px
+                    {formatDistanceToNow(project.updatedAt, {
+                      addSuffix: true,
+                    })}
+                  </TableCell>
+                  <TableCell className="flex items-center justify-end">
+                    <DropdownMenu modal={false}>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          disabled={false}
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="size-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end" className="w-60">
+                        <DropdownMenuItem
+                          className="h-10 cursor-pointer"
+                          // disabled={duplicateMutation.isPending}
+                          // onClick={() => onCopy(project.id)}
+                        >
+                          <CopyIcon className="size-4 mr-2" />
+                          Make a copy
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          className="h-10 cursor-pointer"
+                          // disabled={removeMutation.isPending}
+                          // onClick={() => onDelete(project.id)}
+                        >
+                          <Trash className="size-4 mr-2" />
+                          Delete
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
                   </TableCell>
                 </TableRow>
               ))}
@@ -91,6 +165,17 @@ export const ProjectsSection = () => {
           ))}
         </TableBody>
       </Table>
+      {hasNextPage && (
+        <div className="w-full flex items-center justify-center pt-4">
+          <Button
+            variant="ghost"
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+          >
+            Load more
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
