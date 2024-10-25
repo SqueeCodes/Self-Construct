edf391fdac36132148fc73f6825ac35d61bdf28d
David <itsmesquee@gmail.com>
2024-10-25 16:26:26 -0600

    fixed deployment bug

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,50 +1,56 @@
 "use client";
+
 import { fabric } from "fabric";
 import debounce from "lodash.debounce";
 import { useCallback, useEffect, useRef, useState } from "react";
 
-import { ActiveTool, selectionDependentTools } from "../types";
-import { ResponseType } from "../../projects/api/use-get-project";
+import { ResponseType } from "@/features/projects/api/use-get-project";
+import { useUpdateProject } from "@/features/projects/api/use-update-project";
 
-import { useEditor } from "../hooks/use-editor";
-import { Navbar } from "./navbar";
-import { Sidebar } from "./sidebar";
-import { Toolbar } from "./toolbar";
-import { Footer } from "./footer";
-import { ShapeSidebar } from "./shape-sidebar";
-import { FillColorSidebar } from "./fill-color-sidebar";
-import { StrokeColorSidebar } from "./stroke-color-sidebar";
-import { StrokeWidthSidebar } from "./stroke-width-sidebar";
-import { OpacitySidebar } from "./opacity-sidebar";
-import { TextSidebar } from "./text-sidebar";
-import { FontSidebar } from "./font-sidebar";
-import { ImageSidebar } from "./image-sidebar";
-import { FilterSidebar } from "./filter-sidebar";
-import { AiSidebar } from "./ai-sidebar";
-import { RemoveBgSidebar } from "./remove-bg-sidebar";
-import { DrawSidebar } from "./draw-sidebar";
-import { SettingsSidebar } from "./settings-sidebar";
-import { useUpdateProject } from "../../projects/api/use-update-project";
-import { TemplateSidebar } from "./template-sidebar";
+import { 
+  ActiveTool, 
+  selectionDependentTools
+} from "@/features/editor/types";
+import { Navbar } from "@/features/editor/components/navbar";
+import { Footer } from "@/features/editor/components/footer";
+import { useEditor } from "@/features/editor/hooks/use-editor";
+import { Sidebar } from "@/features/editor/components/sidebar";
+import { Toolbar } from "@/features/editor/components/toolbar";
+import { ShapeSidebar } from "@/features/editor/components/shape-sidebar";
+import { FillColorSidebar } from "@/features/editor/components/fill-color-sidebar";
+import { StrokeColorSidebar } from "@/features/editor/components/stroke-color-sidebar";
+import { StrokeWidthSidebar } from "@/features/editor/components/stroke-width-sidebar";
+import { OpacitySidebar } from "@/features/editor/components/opacity-sidebar";
+import { TextSidebar } from "@/features/editor/components/text-sidebar";
+import { FontSidebar } from "@/features/editor/components/font-sidebar";
+import { ImageSidebar } from "@/features/editor/components/image-sidebar";
+import { FilterSidebar } from "@/features/editor/components/filter-sidebar";
+import { DrawSidebar } from "@/features/editor/components/draw-sidebar";
+import { AiSidebar } from "@/features/editor/components/ai-sidebar";
+import { TemplateSidebar } from "@/features/editor/components/template-sidebar";
+import { RemoveBgSidebar } from "@/features/editor/components/remove-bg-sidebar";
+import { SettingsSidebar } from "@/features/editor/components/settings-sidebar";
 
 interface EditorProps {
   initialData: ResponseType["data"];
-}
+};
 
 export const Editor = ({ initialData }: EditorProps) => {
   const { mutate } = useUpdateProject(initialData.id);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedSave = useCallback(
     debounce(
       (values: { 
-        json: string; 
-        height: number; 
-        width: number 
+        json: string,
+        height: number,
+        width: number,
       }) => {
-      mutate(values);
-    }, 
+        mutate(values);
+    },
     500
   ), [mutate]);
+
   const [activeTool, setActiveTool] = useState<ActiveTool>("select");
 
   const onClearSelection = useCallback(() => {
@@ -57,35 +63,30 @@ export const Editor = ({ initialData }: EditorProps) => {
     defaultState: initialData.json,
     defaultWidth: initialData.width,
     defaultHeight: initialData.height,
-
     clearSelectionCallback: onClearSelection,
     saveCallback: debouncedSave,
   });
 
-  const onChangeActiveTool = useCallback(
-    (tool: ActiveTool) => {
-      if (tool === "draw") {
-        editor?.enableDrawingMode();
-      }
+  const onChangeActiveTool = useCallback((tool: ActiveTool) => {
+    if (tool === "draw") {
+      editor?.enableDrawingMode();
+    }
 
-      if (activeTool === "draw") {
-        editor?.disableDrawingMode();
-      }
+    if (activeTool === "draw") {
+      editor?.disableDrawingMode();
+    }
 
-      if (tool === activeTool) {
-        return setActiveTool("select");
-      }
-
-      setActiveTool(tool);
-    },
-    [activeTool, editor]
-  );
+    if (tool === activeTool) {
+      return setActiveTool("select");
+    }
+    
+    setActiveTool(tool);
+  }, [activeTool, editor]);
 
   const canvasRef = useRef(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log("init");
     const canvas = new fabric.Canvas(canvasRef.current, {
       controlsAboveOverlay: true,
       preserveObjectStacking: true,
@@ -114,91 +115,76 @@ export const Editor = ({ initialData }: EditorProps) => {
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <ShapeSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <FillColorSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <StrokeColorSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <StrokeWidthSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <OpacitySidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <TextSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <FontSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <ImageSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <TemplateSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <FilterSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <AiSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <RemoveBgSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <DrawSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <SettingsSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
-
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
           <Toolbar
             editor={editor}
@@ -206,10 +192,7 @@ export const Editor = ({ initialData }: EditorProps) => {
             onChangeActiveTool={onChangeActiveTool}
             key={JSON.stringify(editor?.canvas.getActiveObject())}
           />
-          <div
-            className="flex-1 h-[calc(100%-124px)] bg-muted"
-            ref={containerRef}
-          >
+          <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
             <canvas ref={canvasRef} />
           </div>
           <Footer editor={editor} />
